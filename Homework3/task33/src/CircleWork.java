708aa1ea4742e73f7e4691c6a37cded60e1f4df5
tiranousor <124511622+tiranousor@users.noreply.github.com>
2023-03-24 12:12:00 +0300

    Homework3

---
+++ Homework3/task33/src/CircleWork.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class CircleWork {
+        public static void main(String[] radcol) {
+            System.out.print("Введите радиус круга");
+            Scanner in = new Scanner(System.in);
+            int radius = in.nextInt();
+            Circle circle = new Circle(radius, Color);
+
+            System.out.format("Площадь: %f \n", circle.area());
+            System.out.format("Периметр: %f\n", circle.perimeter());
+            System.out.println(circle);
+        }
+}
